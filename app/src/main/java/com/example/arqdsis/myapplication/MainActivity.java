77674abd06aaf48fbdc7e0264ef58380df0f3901
arqdsis <arqdsis@mocli10234.animaedu.intranet>
2017-10-26 18:39:48 -0200

    Initial commit

---
+++ app/src/main/java/com/example/arqdsis/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.arqdsis.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+     public void Ligar(){
+
+    }
+
+    }
+}
